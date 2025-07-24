74d947fddd88a5c5a3149a77f3a0fc5ca336bb87
devops-revanth <revanth1226@gmail.com>
2025-07-24 18:48:47 +0530

    shell

--- 7-install.sh
+++ 7-install.sh
@@ -10,3 +10,17 @@ then
 fi
 
 dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dny install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MYSQL ...... FAILURE "
+        exit 1
+    else
+        echo "Installing MYSQL ...... SUCCESS "
+    fi
+else
+    echo "MYSQL is already ...... INSTALLED "
+fi
