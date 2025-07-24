56a90d136747cf9a3dfb339b105c574b54d97532
devops-revanth <revanth1226@gmail.com>
2025-07-24 18:14:42 +0530

    shell

--- 6-conditions.sh
+++ 6-conditions.sh
@@ -2,9 +2,9 @@
 
 A=$1
 
-if [ $A gt 100 ]
+if [ $A -gt 100 ]
 then
-echo "Entered number is greater than 100"
+    echo "Entered number is greater than 100"
 else
-echo "Entered number is less than or equal to 100"
+    echo "Entered number is less than or equal to 100"
 fi
