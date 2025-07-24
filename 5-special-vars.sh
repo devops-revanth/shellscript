d08c3239101f8c25dc032904650e70ad0b34b15f
devops-revanth <revanth1226@gmail.com>
2025-07-24 16:23:23 +0530

    shell

--- 5-special-vars.sh
+++ 5-special-vars.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "All variables passed : $@"
-echo "Number of variables: $#"
+echo "All variables passed: $@"
+echo "Number of variables passed : $#"
 echo "Script Name : $0"
-echo "Present working directory :  $PWD"
+echo "Currect directory : $PWD"
 echo "Home directory of current user : $HOME"
-echo "Which user is running this script : $USER"
-echo "Process id of curret script : $$"
+echo "The user running the script : $USER"
+echo "Process id : $$"
+
 sleep 60 &
-echo "Process id of previous background command : $!"
+echo "Process id of last background command : $!"
