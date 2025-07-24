5bdff088da6f3790f7c44f25d36f28be716ab28c
devops-revanth <revanth1226@gmail.com>
2025-07-24 16:04:05 +0530

    shell

---
+++ 4-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Movies=("pushpa" "rrr" "raju")
+
+echo "First movie : ${Movies[0]}"
+echo "Second movie : ${Movies[1]}"
+echo "Third movie : ${Movies[2]}"
+
+echo "All movies are : ${Movies[@]}"
