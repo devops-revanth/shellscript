10ad7252aa38437b6b23d4458b8c7470f870c431
devops-revanth <revanth1226@gmail.com>
2025-07-24 13:10:00 +0530

    variable

---
+++ 1-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+A=$1
+B=$2
+
+echo " $A :: Hello $B"
+echo " $B :: Hello $A"
+echo " $A :: How are you $B?"
+echo " $B :: I am fine, thanky you $A ,  how are you?"
+echo " $A :: I am fine too, thank you $B"
