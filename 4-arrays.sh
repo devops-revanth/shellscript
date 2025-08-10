84ce77778519cfc2e7aba042fafa5f8f6fb98c71
devops-revanth <revanth1226@gmail.com>
2025-08-11 04:23:53 +0530

    shell

--- 4-arrays.sh
+++ 4-arrays.sh
@@ -6,6 +6,8 @@ echo "First movie is : ${Movies[0]}"
 echo "Second movie is : ${Movies[1]}"
 echo "Third movies is : ${Movies[2]}"
 
+echo "First movie is : $Movies[0]"
+
 echo "Now lets see all movies are below"
 
 echo "All movies list is : ${Movies[@]}"
