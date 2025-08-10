ca494b7432b3ff573ea91687364321c38c8e13c8
devops-revanth <revanth1226@gmail.com>
2025-08-11 04:36:25 +0530

    shell

--- 4-arrays.sh
+++ 4-arrays.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-Movies=("RRR" "GameChanger" "Devara")
+# Movies=("RRR" "GameChanger" "Devara")
+# 
+# echo "First movie is : ${Movies[0]}"
+# echo "Second movie is : ${Movies[1]}"
+# echo "Third movies is : ${Movies[2]}"
+# 
+# echo "First movie is : $Movies[1]"
+# 
+# echo "Now lets see all movies are below"
+# 
+# echo "All movies list is : ${Movies[@]}"
 
-echo "First movie is : ${Movies[0]}"
-echo "Second movie is : ${Movies[1]}"
-echo "Third movies is : ${Movies[2]}"
 
-echo "First movie is : $Movies[1]"
+Course=("Linux" "Shell" "Ansible")
 
-echo "Now lets see all movies are below"
+echo "Course First topic is : ${Course[0]}"
+echo "Course Second topic is : ${Course[1]}"
+echo "Course Third topic is : ${Course[2]}"
 
-echo "All movies list is : ${Movies[@]}"
+Course+=("Git")
+
+echo "Course added , fourth topic is : ${Course[3]}"
+
+echo "All topics are : ${Course[@]}"
