e7146e305006cf5406141422cafccf262db5a58b
devops-revanth <revanth1226@gmail.com>
2025-08-15 04:26:09 +0530

    shell

--- 02-Install.sh
+++ 02-Install.sh
@@ -9,7 +9,8 @@ N="\e[0m"
 echo -e "User who is running this script is : $G $USER $N"
 
 USERID=$(id -u)
-if [ $USERID -ne 0]
+
+if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR $N You need to run this script as sudo or root user"
     exit 1
