999229f63e8f26dbe9928d9563af07f8919f3fd7
devops-revanth <revanth1226@gmail.com>
2025-08-15 04:09:36 +0530

    message

---
+++ 01-Allshell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+today = $(date +%A)
+
+if [ $today -eq Sunday ]
+then
+    echo "$today is Holiday , no need to go to school"
+else
+    echo "Today is not Sunday , need to go to school"
+fi
