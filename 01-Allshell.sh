8d12ad0000fb5835569d324266ddb22bfdf4d5b8
devops-revanth <revanth1226@gmail.com>
2025-08-15 04:18:57 +0530

    shell

--- 01-Allshell.sh
+++ 01-Allshell.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-today=$(date +%A)
+# today=$(date +%A)
 
-if [[ $today == 'Sunday' ]]
+# if [[ $today == 'Sunday' ]]
+# then
+#     echo "$today is Holiday , no need to go to school"
+# else
+#     echo "Today is not Sunday , need to go to school"
+# fi
+
+
+number=$1
+if [ $number -gt 100 ]
 then
-    echo "$today is Holiday , no need to go to school"
+    echo "$number is greater than 100"
 else
-    echo "Today is not Sunday , need to go to school"
+    echo "$number is less than or equal to 100"
+
 fi
